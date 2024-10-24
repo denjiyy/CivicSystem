eb960f20bbc6ca02341e2106fe7941a23c7d14ef
Denis <125385668+denjiyy@users.noreply.github.com>
2024-10-24 22:45:50 +0300

    Add files via upload

---
+++ Controllers/SuggestionController.cs
@@ -0,0 +1,36 @@
+﻿using CivicSystem.Data;
+using CivicSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace CivicSystem.Controllers;
+
+[Authorize]
+public class SuggestionController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public SuggestionController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    [AllowAnonymous]
+    public async Task<IActionResult> Index()
+    {
+        var suggestions = await _context.Suggestions
+            .Select(s => new SuggestionInfoViewModel(
+                s.Id,
+                s.Title,
+                s.Description,
+                s.UserId,
+                s.Status.ToString(),
+                s.CreatedOn.ToString()
+            )).ToListAsync();
+
+        return View(suggestions);
+    }
+}
