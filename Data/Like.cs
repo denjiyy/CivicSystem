8c0ecaab7c27b7f5597bbc86e2f18a199edc4f49
Denis <125385668+denjiyy@users.noreply.github.com>
2024-10-24 22:44:23 +0300

    Add files via upload

---
+++ Data/Like.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CivicSystem.Data
+{
+    [PrimaryKey(nameof(UserId), nameof(SuggestionId))]
+    public class Like
+    {
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+        [ForeignKey(nameof(UserId))]
+        public IdentityUser User { get; set; } = null!;
+
+        [Required]
+        public Guid SuggestionId { get; set; }
+        [ForeignKey(nameof(SuggestionId))]
+        public Suggestion Suggestion { get; set; } = null!;
+    }
+}
