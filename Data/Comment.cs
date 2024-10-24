0db11d6337f09bd6c50a1efda388b609b34ad3f9
Denis <125385668+denjiyy@users.noreply.github.com>
2024-10-24 22:43:59 +0300

    Add files via upload

---
+++ Data/Comment.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static CivicSystem.Data.DataConstants;
+
+namespace CivicSystem.Data
+{
+    public class Comment
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MinLength(CommentMinLength)]
+        [MaxLength(CommentMaxLength)]
+        [Comment(CommentDbComment)]
+        public string CommentText { get; set; } = string.Empty;
+        
+        [Required]
+        [Comment(CommentCreatedOnComment)]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        [Comment(SuggestionIdComment)]
+        public Guid SuggestionId { get; set; }
+        [ForeignKey(nameof(SuggestionId))]
+        public Suggestion Suggestion { get; set; } = null!;
+        
+        public ICollection<Vote> Votes { get; set; } = new HashSet<Vote>();
+    }
+}
