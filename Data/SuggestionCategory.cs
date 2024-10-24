4d49a7fe7457031d572db80b53f5b70e9bbcbea3
Denis <125385668+denjiyy@users.noreply.github.com>
2024-10-24 22:44:43 +0300

    Add files via upload

---
+++ Data/SuggestionCategory.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace CivicSystem.Data;
+
+[PrimaryKey(nameof(SuggestionId), nameof(CategoryId))]
+public class SuggestionCategory
+{
+    [Required]
+    public Guid SuggestionId { get; set; }
+    [ForeignKey(nameof(SuggestionId))]
+    public Suggestion Suggestion { get; set; } = null!;
+    
+    [Required]
+    public Guid CategoryId { get; set; }
+    [ForeignKey(nameof(CategoryId))]
+    public Category Category { get; set; } = null!;
+}
