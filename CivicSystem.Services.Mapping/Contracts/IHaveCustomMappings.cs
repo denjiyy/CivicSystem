4add36f722d3ce8c83c421126b6ac0b635d629f3
Denis <125385668+denjiyy@users.noreply.github.com>
2024-10-26 20:11:52 +0300

    Separated projects

---
+++ CivicSystem.Services.Mapping/Contracts/IHaveCustomMappings.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace CivicSystem.Services.Mapping.Contracts
+{
+    public interface IHaveCustomMappings
+    {
+        void CreateMappings(IProfileExpression configuration);
+    }
+}
---
+++ CivicSystem.Services.Mapping/QueryableMappingExtensions.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CivicSystem.Services.Mapping
+{
+    public static class QueryableMappingExtensions
+    {
+        public static IQueryable<TDestination> To<TDestination>(
+            this IQueryable<TDestination> source,
+            params Expression<Func<TDestination, object>>[] membersToExpand)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return source.ProjectTo(AutoMapperConfig.MapperInstance.ConfigurationProvider, null, membersToExpand);
+        }
+
+        public static IQueryable<TDestination> To<TDestination>(
+            this IQueryable source,
+            object parameters)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return source.ProjectTo<TDestination>(AutoMapperConfig.MapperInstance.ConfigurationProvider, parameters);
+        }
+    }
+}
